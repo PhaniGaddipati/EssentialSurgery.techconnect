849df24d2df4a0d63d4eb0c9259aa46ee62a0228
Phani Gaddipati <phanigaddipati@gmail.com>
2018-02-23 20:14:56 -0500

    different messages for finished status

--- app/src/main/java/org/techconnect/asynctasks/ExportResponsesAsyncTask.java
+++ app/src/main/java/org/techconnect/asynctasks/ExportResponsesAsyncTask.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 
 import org.techconnect.R;
+import org.techconnect.model.session.Session;
 import org.techconnect.sql.TCDatabaseHelper;
 
 /**
@@ -25,10 +26,14 @@ public class ExportResponsesAsyncTask extends AsyncTask<String, Void, Integer> {
     protected Integer doInBackground(String... strings) {
         //Get generated string
         String resp = message;
+        int sub_id = R.string.exportResponse_subject;
         if (resp == null) {
             if (strings.length > 0) {
+                Session sesh = TCDatabaseHelper.get(context).getSession(strings[0]);
                 String r = TCDatabaseHelper.get(context).writeResponsesToString(strings[0]);
-                resp = String.format("%s\n%s", context.getString(R.string.exportResponse_autofill), r);
+                int msg_id = sesh.isFinished() ? R.string.exportResponse_autofill_finished : R.string.exportResponse_autofill;
+                sub_id = sesh.isFinished() ? R.string.exportResponse_subject_finished : R.string.exportResponse_subject;
+                resp = String.format("%s\n%s", context.getString(msg_id), r);
             } else {
                 resp = context.getString(R.string.exportResponse_autofill);
             }
@@ -38,7 +43,7 @@ public class ExportResponsesAsyncTask extends AsyncTask<String, Void, Integer> {
             //Send email based on String arguments
             Intent emailIntent = new Intent(Intent.ACTION_SEND);
             emailIntent.putExtra(Intent.EXTRA_TEXT, resp);
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.exportResponse_subject));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(sub_id));
             emailIntent.setType("text/plain");
             context.startActivity(Intent.createChooser(emailIntent, "Select App"));
             return 1;
